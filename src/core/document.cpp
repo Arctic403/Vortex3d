8f8a2bf4fb9caf4f3af4e36d754b6035242230a8
Arctic403 <kc.campbell132@gmail.com>
2026-09-03 23:55:50 -0400

    core: add scenes collections make-unique and change events

--- src/core/document.cpp
+++ src/core/document.cpp
@@ -6,9 +6,39 @@ namespace vortex {
 
 Document::Document() : id_(DocumentId{1}), nextId_(2) {}
 
+SceneId Document::createScene(std::string name) {
+    const SceneId sceneId = allocateId<SceneId>();
+    const CollectionId rootId = allocateId<CollectionId>();
+
+    scenes_.emplace(sceneId, SceneBlock{sceneId, std::move(name), rootId, 1});
+    collections_.emplace(rootId, CollectionBlock{rootId, "Scene Collection", sceneId, {}, {}, 1});
+
+    markChanged(DataKind::Scene, ChangeKind::Created, sceneId.value());
+    markChanged(DataKind::Collection, ChangeKind::Created, rootId.value());
+    return sceneId;
+}
+
+CollectionId Document::createCollection(const SceneId sceneId, std::string name, const CollectionId parentId) {
+    const auto sceneIt = scenes_.find(sceneId);
+    if (sceneIt == scenes_.end()) {
+        return {};
+    }
+
+    const CollectionId resolvedParent = parentId ? parentId : sceneIt->second.rootCollectionId;
+    if (!collectionBelongsToScene(resolvedParent, sceneId)) {
+        return {};
+    }
+
+    const CollectionId id = allocateId<CollectionId>();
+    collections_.emplace(id, CollectionBlock{id, std::move(name), sceneId, resolvedParent, {}, 1});
+    markChanged(DataKind::Collection, ChangeKind::Created, id.value());
+    return id;
+}
+
 MeshId Document::createMesh(std::string name) {
     const MeshId id = allocateId<MeshId>();
     meshes_.emplace(id, MeshBlock{id, std::move(name), 1});
+    markChanged(DataKind::Mesh, ChangeKind::Created, id.value());
     return id;
 }
 
@@ -19,7 +49,16 @@ ObjectId Document::createObject(std::string name, const MeshId meshId) {
 
     const ObjectId id = allocateId<ObjectId>();
     objects_.emplace(id, ObjectBlock{id, std::move(name), meshId, {}, 1});
+    markChanged(DataKind::Object, ChangeKind::Created, id.value());
     return id;
+}
+
+bool Document::hasScene(const SceneId id) const noexcept {
+    return id && scenes_.contains(id);
+}
+
+bool Document::hasCollection(const CollectionId id) const noexcept {
+    return id && collections_.contains(id);
 }
 
 bool Document::hasMesh(const MeshId id) const noexcept {
@@ -28,6 +67,16 @@ bool Document::hasMesh(const MeshId id) const noexcept {
 
 bool Document::hasObject(const ObjectId id) const noexcept {
     return id && objects_.contains(id);
+}
+
+const SceneBlock* Document::scene(const SceneId id) const noexcept {
+    const auto it = scenes_.find(id);
+    return it == scenes_.end() ? nullptr : &it->second;
+}
+
+const CollectionBlock* Document::collection(const CollectionId id) const noexcept {
+    const auto it = collections_.find(id);
+    return it == collections_.end() ? nullptr : &it->second;
 }
 
 const MeshBlock* Document::mesh(const MeshId id) const noexcept {
@@ -54,6 +103,7 @@ bool Document::setObjectMesh(const ObjectId objectId, const MeshId meshId) {
 
     objectIt->second.meshId = meshId;
     ++objectIt->second.revision;
+    markChanged(DataKind::Object, ChangeKind::Updated, objectId.value());
     return true;
 }
 
@@ -74,6 +124,72 @@ bool Document::setObjectParent(const ObjectId objectId, const ObjectId parentId)
 
     objectIt->second.parentId = parentId;
     ++objectIt->second.revision;
+    markChanged(DataKind::Object, ChangeKind::Updated, objectId.value());
+    return true;
+}
+
+MeshId Document::makeObjectMeshUnique(const ObjectId objectId) {
+    const auto objectIt = objects_.find(objectId);
+    if (objectIt == objects_.end() || !objectIt->second.meshId) {
+        return {};
+    }
+
+    const MeshId sourceId = objectIt->second.meshId;
+    if (meshUserCount(sourceId) <= 1) {
+        return sourceId;
+    }
+
+    const auto sourceIt = meshes_.find(sourceId);
+    if (sourceIt == meshes_.end()) {
+        return {};
+    }
+
+    const MeshId cloneId = allocateId<MeshId>();
+    MeshBlock clone = sourceIt->second;
+    clone.id = cloneId;
+    clone.revision = 1;
+    meshes_.emplace(cloneId, std::move(clone));
+
+    objectIt->second.meshId = cloneId;
+    ++objectIt->second.revision;
+
+    markChanged(DataKind::Mesh, ChangeKind::Created, cloneId.value());
+    markChanged(DataKind::Object, ChangeKind::Updated, objectId.value());
+    return cloneId;
+}
+
+bool Document::linkObjectToCollection(const ObjectId objectId, const CollectionId collectionId) {
+    if (!hasObject(objectId)) {
+        return false;
+    }
+
+    const auto collectionIt = collections_.find(collectionId);
+    if (collectionIt == collections_.end()) {
+        return false;
+    }
+
+    const auto [_, inserted] = collectionIt->second.objectIds.insert(objectId);
+    if (!inserted) {
+        return true;
+    }
+
+    ++collectionIt->second.revision;
+    markChanged(DataKind::Collection, ChangeKind::Linked, collectionId.value());
+    return true;
+}
+
+bool Document::unlinkObjectFromCollection(const ObjectId objectId, const CollectionId collectionId) {
+    const auto collectionIt = collections_.find(collectionId);
+    if (collectionIt == collections_.end()) {
+        return false;
+    }
+
+    if (collectionIt->second.objectIds.erase(objectId) == 0) {
+        return false;
+    }
+
+    ++collectionIt->second.revision;
+    markChanged(DataKind::Collection, ChangeKind::Unlinked, collectionId.value());
     return true;
 }
 
@@ -88,10 +204,19 @@ bool Document::removeObject(const ObjectId id) {
         if (child.parentId == id) {
             child.parentId = {};
             ++child.revision;
+            markChanged(DataKind::Object, ChangeKind::Updated, child.id.value());
+        }
+    }
+
+    for (auto& [collectionId, collection] : collections_) {
+        if (collection.objectIds.erase(id) != 0) {
+            ++collection.revision;
+            markChanged(DataKind::Collection, ChangeKind::Unlinked, collectionId.value());
         }
     }
 
     objects_.erase(it);
+    markChanged(DataKind::Object, ChangeKind::Removed, id.value());
     return true;
 }
 
@@ -102,7 +227,34 @@ bool Document::removeMesh(const MeshId id) {
     }
 
     meshes_.erase(it);
+    markChanged(DataKind::Mesh, ChangeKind::Removed, id.value());
     return true;
+}
+
+std::size_t Document::meshUserCount(const MeshId meshId) const noexcept {
+    std::size_t users = 0;
+    for (const auto& [objectId, object] : objects_) {
+        (void)objectId;
+        if (object.meshId == meshId) {
+            ++users;
+        }
+    }
+    return users;
+}
+
+std::vector<ChangeEvent> Document::changesSince(const std::uint64_t revision) const {
+    std::vector<ChangeEvent> result;
+    for (const ChangeEvent& event : changes_) {
+        if (event.revision > revision) {
+            result.push_back(event);
+        }
+    }
+    return result;
+}
+
+void Document::markChanged(const DataKind dataKind, const ChangeKind changeKind, const std::uint64_t entityId) {
+    ++revision_;
+    changes_.push_back(ChangeEvent{revision_, dataKind, changeKind, entityId});
 }
 
 bool Document::wouldCreateParentCycle(const ObjectId objectId, ObjectId parentId) const noexcept {
@@ -131,20 +283,41 @@ bool Document::wouldCreateParentCycle(const ObjectId objectId, ObjectId parentId
     return false;
 }
 
-std::size_t Document::meshUserCount(const MeshId meshId) const noexcept {
-    std::size_t users = 0;
-    for (const auto& [objectId, object] : objects_) {
-        (void)objectId;
-        if (object.meshId == meshId) {
-            ++users;
-        }
-    }
-    return users;
+bool Document::collectionBelongsToScene(const CollectionId collectionId, const SceneId sceneId) const noexcept {
+    const auto it = collections_.find(collectionId);
+    return it != collections_.end() && it->second.sceneId == sceneId;
 }
 
 bool Document::validate() const noexcept {
     if (!id_) {
         return false;
+    }
+
+    for (const auto& [sceneId, scene] : scenes_) {
+        if (!sceneId || scene.id != sceneId) {
+            return false;
+        }
+        const auto rootIt = collections_.find(scene.rootCollectionId);
+        if (rootIt == collections_.end() || rootIt->second.sceneId != sceneId || rootIt->second.parentId) {
+            return false;
+        }
+    }
+
+    for (const auto& [collectionId, collection] : collections_) {
+        if (!collectionId || collection.id != collectionId || !hasScene(collection.sceneId)) {
+            return false;
+        }
+        if (collection.parentId) {
+            const auto parentIt = collections_.find(collection.parentId);
+            if (parentIt == collections_.end() || parentIt->second.sceneId != collection.sceneId) {
+                return false;
+            }
+        }
+        for (const ObjectId objectId : collection.objectIds) {
+            if (!hasObject(objectId)) {
+                return false;
+            }
+        }
     }
 
     for (const auto& [meshId, mesh] : meshes_) {
